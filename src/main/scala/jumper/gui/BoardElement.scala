244ea5b2fbdc0834cde2af617f3097db96377c57
Wojciech Przybytek <80359324+wprzybytek@users.noreply.github.com>
2022-06-21 14:42:33 +0200

    Gitignore fix

--- src/main/scala/jumper/gui/BoardElement.scala
+++ src/main/scala/jumper/gui/BoardElement.scala
@@ -1,21 +1,21 @@
-package jumper.gui
-
-import scalafx.geometry.Pos
-import scalafx.scene.image.ImageView
-import scalafx.scene.layout.VBox
-
-class BoardElement(val row: Int, val column: Int) extends VBox{
-
-  alignment = Pos.Center
-  children = createImage()
-
-  def createImage(): ImageView = {
-    val srcImg: String = if (pawn.isWhite) "white.png" else "black.png"
-    val image = new ImageView(srcImg)
-    image.setFitWidth(50)
-    image.setFitHeight(50)
-    image
-  }
-
-
-}
+//package jumper.gui
+//
+//import scalafx.geometry.Pos
+//import scalafx.scene.image.ImageView
+//import scalafx.scene.layout.VBox
+//
+//class BoardElement(val row: Int, val column: Int) extends VBox{
+//
+//  alignment = Pos.Center
+//  children = createImage()
+//
+//  def createImage(): ImageView = {
+//    val srcImg: String = if (pawn.isWhite) "white.png" else "black.png"
+//    val image = new ImageView(srcImg)
+//    image.setFitWidth(50)
+//    image.setFitHeight(50)
+//    image
+//  }
+//
+//
+//}
